1eb94e71f2424abeaaf485bac5bde73ab487b13e
Rahelassefa <ritaeagle1@gmail.com>
2022-04-07 13:19:10 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main : entry point
+ *
+ * Description: prints "with proper grammar, but the outcome is a piece of art"
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	printf ("\with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
