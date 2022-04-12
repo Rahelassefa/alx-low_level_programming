e9b989976e2c23c3a3ab9e994a05d543f8c9eb4c
Rahelassefa <ritaeagle1@gmail.com>
2022-04-12 14:54:21 -0700

    100-times

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,4 +12,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
 #endif /*_MAIN_H_8*/
