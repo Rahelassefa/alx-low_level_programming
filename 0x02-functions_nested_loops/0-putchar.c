650c9edb67e9c9c2971df9709da3e63b12e25aeb
Rahelassefa <ritaeagle1@gmail.com>
2022-04-12 11:43:35 -0700

    puts

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,13 +5,15 @@
  * Return: 0
  */
 int main(void)
+{
 	char alx[] = "_putchar";
 
 	int c;
 
 	for ( c = 0; c < 8; c++)
-{
-	_putchar(alx[c]);
+	{
+		_putchar(alx[c]);
+	}
+	_putchar('/n');
+	return 0;
 }
-_putchar("/n");
-return 0;
