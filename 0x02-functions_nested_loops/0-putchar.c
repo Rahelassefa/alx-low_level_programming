b898414cf2799c2f66da2f0a79e660c73004950f
Rahelassefa <ritaeagle1@gmail.com>
2022-04-12 11:31:24 -0700

    puts

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+ */
+int main(void)
+	char alx[] = "_putchar";
+
+	int c;
+
+	for ( c = 0; c < 8; c++)
+{
+	_putchar(alx[c]);
+}
+_putchar("/n");
+return 0;
