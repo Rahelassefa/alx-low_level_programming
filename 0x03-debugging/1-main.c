ffc3aa9f4a380a42b8784d4b2a1194dda99e8861
Rahelassefa <ritaeagle1@gmail.com>
2022-04-13 11:43:42 -0700

    main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,13 +12,13 @@ int main(void)
 
 	i = 0;
 
-/**
+/*
  * while (i < 10)
  * {
+ * Infinite loop - No increase of variable
  * putchar(i);
  * }
  */
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
