3c19257c6f516c1781c551310406d70c52038523
Rahelassefa <101384486+Rahelassefa@users.noreply.github.com>
2022-04-20 09:46:52 +0300

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * string_toupper - changes all lowercase of string to uppercase
- * @s: string array
- * Return: string array
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
+ * @s: string to modify
+ *
+ * Return: the resulting string
  */
-
 char *string_toupper(char *s)
 {
 	int i;
