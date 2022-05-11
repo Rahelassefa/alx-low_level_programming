05a0f770d0fb15c8a843292e0182cbf3bd92ee44
Rahelassefa <ritaeagle1@gmail.com>
2022-05-11 11:03:06 -0700

    commit

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,8 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif 
