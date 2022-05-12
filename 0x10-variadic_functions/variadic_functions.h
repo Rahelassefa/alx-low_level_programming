7edd15d6ee9301c7f6ff552ae5b61e232df9e353
Rahelassefa <ritaeagle1@gmail.com>
2022-05-12 11:01:30 -0700

    commit

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif 
