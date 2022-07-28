eb5dbd1b441ceda1f448558cf3e296ebaca3e60b
Rahelassefa <101384486+Rahelassefa@users.noreply.github.com>
2022-07-28 17:18:06 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,16 +1,65 @@
 #include "hash_tables.h"
 
 /**
- * key_index - function that generates a distributed index
- * according to a given hash
+ * add_n_hash - adds a node at the beginning of a hash at a given index
  *
- * @key: key passed
- * @size: size of the hash tables
- * Return: key index
+ * @head: head of the hash linked list
+ * @key: key of the hash
+ * @value: value to store
+ * Return: head of the hash
  */
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
+hash_node_t *add_n_hash(hash_node_t **head, const char *key, const char *value)
 {
-	if (key == NULL || size == 0)
+	hash_node_t *tmp;
+
+	tmp = *head;
+
+	while (tmp != NULL)
+	{
+		if (strcmp(key, tmp->key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (*head);
+		}
+		tmp = tmp->next;
+	}
+
+	tmp = malloc(sizeof(hash_node_t));
+
+	if (tmp == NULL)
+		return (NULL);
+
+	tmp->key = strdup(key);
+	tmp->value = strdup(value);
+	tmp->next = *head;
+	*head = tmp;
+
+	return (*head);
+}
+
+/**
+ * hash_table_set - adds a hash (key, value) to a given hash table
+ *
+ * @ht: pointer to the hash table
+ * @key: key of the hash
+ * @value: value to store
+ * Return: 1 if successes, 0 if fails
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int k_index;
+
+	if (ht == NULL)
 		return (0);
-	return (hash_djb2(key) % size);
+
+	if (key == NULL || *key == '\0')
+		return (0);
+
+	k_index = key_index((unsigned char *)key, ht->size);
+
+	if (add_n_hash(&(ht->array[k_index]), key, value) == NULL)
+		return (0);
+
+	return (1);
 }
